b150c5859ac33644766cccfa8a3bff2ad9a43c02
Victor <victornwowo7@gmail.com>
2024-12-30 20:15:39 +0100

    more content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Components/NavBar/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Section from "./Components/Sections/Section";
 
 const App = () => {
